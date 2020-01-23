8b3dca07c97e3c3072bccc700a3d1e7116beaafc
Neil Davies <ndavies30@dxc.com>
2020-01-23 09:58:03 +0000

    check working hours

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-
+import time
+from datetime import datetime as dt
 
 hosts = r'C:\Windows\System32\drivers\etc\hosts' # r infront of string - passing row string to avoid break lines
-direct_to = '127.0.0.1' # localhost
+direct_to = '127.0.0.1'
 blocked_sites = ['www.linkedin.com', 'linkedin.com']
 
-file = open('C:\Windows\System32\drivers\etc\hosts.txt')
-
 while True:
-    print(1)
-    
+    if dt(dt.now().year, dt.now().month, dt.now().day, 7) > dt.now() > dt(dt.now().year, dt.now().month, dt.now().day, 15):
+        print('Working...')
+
+        time.sleep(5) # script executes every 5 seconds for testing / change to 300 on completion
